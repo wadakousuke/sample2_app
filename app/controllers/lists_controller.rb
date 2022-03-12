2efbadce425f794f5734de22eeae9287e20585c9
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-12 08:00:33 +0000

    [Finish 7章削除機能の実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -29,6 +29,12 @@ class ListsController < ApplicationController
 
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to "/lists"
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
